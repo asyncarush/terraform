43516f02a9ef11897c3d6d3ecf0e2c124223b5d8
Arush Sharma <arush.sharma@xenonstack>
2025-02-21 19:25:32 +0530

    data-pipeline complete

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -34,3 +34,68 @@ module "ecs" {
   vpc_cidr           = module.vpc.cidr_block
   private_subnet_ids = module.subnet.private_subnet_ids
 }
+
+module "dynamodb" {
+  source = "../../modules/data-pipeline/dynamodb"
+  
+  table_name     = "lifedata-dev"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+  environment    = "dev"
+}
+
+module "s3" {
+  source = "../../modules/data-pipeline/s3"
+  
+  bucket_name  = "lifedata-data-pipeline-dev"
+  environment  = "dev"
+  
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.subnet.private_subnet_ids
+  vpc_cidr_block     = module.vpc.cidr_block
+}
+
+module "iam" {
+  source = "../../modules/data-pipeline/iam"
+  
+  role_name     = "lifedata-pipeline-role-dev"
+  environment   = "dev"
+  
+  dynamodb_table_arns   = [module.dynamodb.table_arn]
+  s3_bucket_arns        = [module.s3.bucket_arn]
+  
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.subnet.private_subnet_ids
+  vpc_cidr_block     = module.vpc.cidr_block
+}
+
+module "lambda" {
+  source = "../../modules/data-pipeline/lambda"
+  
+  function_name     = "lifedata-transformation-dev"
+  lambda_role_arn   = module.iam.role_arn
+  lambda_zip_path   = "../../lambda/transformation.zip"
+  environment       = "dev"
+  
+  vpc_id             = module.vpc.vpc_id
+  vpc_cidr             = module.vpc.cidr_block
+  private_subnet_ids = module.subnet.private_subnet_ids
+  vpc_security_group_id = module.iam.role_id
+}
+
+module "kinesis" {
+  source = "../../modules/data-pipeline/kinesis"
+  
+  stream_name     = "lifedata-stream-dev"
+  firehose_name   = "lifedata-firehose-dev"
+  environment     = "dev"
+  
+  lambda_role_arn   = module.iam.role_arn
+  s3_bucket_arn     = module.s3.bucket_arn
+  lambda_arn        = module.lambda.lambda_function_arn
+  
+  vpc_id             = module.vpc.vpc_id
+  vpc_cidr             = module.vpc.cidr_block
+  private_subnet_ids = module.subnet.private_subnet_ids
+  vpc_security_group_id = module.iam.role_id
+}
